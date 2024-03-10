0039d5380cac8855cc55b929098b8f0610fe937d
cassiebeisheim <40363436+cassiebeisheim@users.noreply.github.com>
2024-03-09 19:09:33 -0600

    Add files via upload

---
+++ version1.py
@@ -0,0 +1,14 @@
+def fizzbuzz():
+    for i in range(1, 101):
+        result = ""
+        if i % 3 == 0:
+            result = "Fizz"
+            if i % 5 == 0:
+                result += "Buzz"
+        elif i % 5 == 0:
+            result = "Buzz"
+        else:
+            result = str(i)
+        print(result)
+
+fizzbuzz()
